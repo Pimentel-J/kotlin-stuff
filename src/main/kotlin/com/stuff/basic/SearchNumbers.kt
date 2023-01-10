70431db337cbdecebd2711e0141c505cf994327f
Pimentel <110205488+Pimentel-J@users.noreply.github.com>
2023-01-10 19:56:07 +0000

    Create SearchNumbers.kt

---
+++ src/main/kotlin/com/stuff/basic/SearchNumbers.kt
@@ -0,0 +1,51 @@
+package com.stuff.basic
+
+/**
+ * A set of functions to search for numbers, according to a certain criteria
+ */
+class SearchNumbers() {
+
+    /**
+     * Determines the highest integer of a given list
+     *
+     * @param numbers list of integers
+     * @return highest integer
+     */
+    fun findMax(numbers: List<Int>): Int {
+        var maxFound = numbers[0]
+        numbers.forEach {
+            maxFound = when (it > maxFound) {
+                true -> it
+                else -> maxFound
+            }
+        }
+        return maxFound
+    }
+
+    /**
+     * Searches a list of integers for the selected numbers and if the order is the same
+     *
+     * @param numbers   list of integers
+     * @param numbersToSearch  list of integers, with the numbers to search for
+     *
+     * @return true if the numbers exist in the list and in the same order, and false otherwise
+     */
+    fun findNumbers(numbers: List<Int>, numbersToSearch: List<Int>): Boolean {
+        var found = false
+        var n = 0
+
+        numbersToSearch.forEach { seqNum ->
+            found = false
+
+            for (i in n until numbers.size) {
+                if (numbers[i] == seqNum) {
+                    found = true
+                    n += numbers.indexOf(numbers[i]) + 1
+                    break
+                }
+            }
+        }
+        return found
+    }
+
+}
